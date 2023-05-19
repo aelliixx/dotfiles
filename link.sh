3ae9a288cbbaa0d7ec8d4380401859d4134aafbc
Donathan <mockus49@gmail.com>
2023-05-19 16:58:42 +0200

    rm links before linking

--- link.sh
+++ link.sh
@@ -2,10 +2,14 @@
 
 echo "Creating symbolic links"
 cd ~
+rm .zshrc
 ln -sf .dotfiles/.zshrc .zshrc
 cd ~/.config
+rm nvim
+rm coc
 ln -sf ~/.dotfiles/nvim nvim
 ln -sf ~/.dotfiles/coc coc
 cd ~/.local/share
+rm nvim
 ln -sf ~/.dotfiles/share/nvim nvim
 
