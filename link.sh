d539a7c5361621bb6b820679c401b9be591d5e2c
Donathan <mockus49@gmail.com>
2023-05-19 15:20:51 +0200

    Relative coc paths

--- link.sh
+++ link.sh
@@ -4,7 +4,8 @@ echo "Creating symbolic links"
 cd ~
 ln -sf .dotfiles/.zshrc .zshrc
 cd ~/.config
-ln -sf ../.dotfiles/nvim nvim
-ln -sf ../.dotfiles/coc coc
+ln -sf ~/.dotfiles/nvim nvim
+ln -sf ~/.dotfiles/coc coc
 cd ~/.local/share
 ln -sf ~/.dotfiles/share/nvim nvim
+
