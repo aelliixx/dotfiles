3fb39a8eab44a4de448c95541ae9e7bf6f98a223
Donathan <mockus49@gmail.com>
2023-05-19 12:42:31 +0200

    Link .zshrc

--- install-zsh.sh
+++ install-zsh.sh
@@ -2,6 +2,7 @@
 
 echo "Installing zsh"
 sudo apt install zsh
+cd ~
+ln -sf .dotfiles/.zshrc .zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
