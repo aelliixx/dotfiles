02c2b2620fbf96174aa72ede6a9ad766c4f55417
Donathan <mockus49@gmail.com>
2023-05-19 12:29:40 +0200

    Update installation order, globally link nvim

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 echo "Installing zsh"
 sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing neovim"
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x nvim.appimage && ./nvim.appimage
+chmod u+x nvim.appimage && ./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -sf /squashfs-root/AppRun /usr/bin/nvim
 
 echo "Installing nvim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -16,5 +17,6 @@ echo "Creating symbolic links"
 cd ~
 ln -sf .dotfiles/.zshrc .zshrc
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd .config
 ln -sf ../.dotfiles/nvim nvim
