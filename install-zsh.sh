2c078a9e34bf21405cf45a332ff020231e89b768
Donathan <mockus49@gmail.com>
2023-05-19 12:39:47 +0200

    Install ohmyzsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -2,5 +2,6 @@
 
 echo "Installing zsh"
 sudo apt install zsh
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
